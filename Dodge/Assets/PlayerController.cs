c17953a5f977bbcad7b4c7f45dd034b3ec041b7a
gwijong <drophail@daum.net>
2022-01-24 12:41:28 +0900

    20220124

---
+++ Dodge/Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRigidbody;
+    public float speed = 8f;
+
+    void Start()
+    {
+        
+    }
+
+   
+    //감지하지 못할 정도의 속도로 갱신됨
+    void Update()
+    {
+        
+    }
+}
