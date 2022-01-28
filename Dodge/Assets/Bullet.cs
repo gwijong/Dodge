5e7ca3b3520d69a0f7c13374e9c2494beca6ed96
gwijong <drophail@daum.net>
2022-01-28 12:44:11 +0900

    20220128
    20220128

--- Dodge/Assets/Bullet.cs
+++ Dodge/Assets/Bullet.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 8f;
-    private Rigidbody bulletRigidbody;
+    public float speed = 8f;  //탄알 속력
+    private Rigidbody bulletRigidbody;  // 이동에 사용할 리지드바디 컴포넌트
 
     void Start()
     {
@@ -17,8 +17,16 @@ public class Bullet : MonoBehaviour
         Destroy(gameObject, 3f);
     }
 
-    void Update()
+    private void OnTriggerEnter(Collider other) //트리거 충돌 시 자동으로 실행되는 메서드
     {
-        
+        if(other.tag == "Player")//충돌한 상대방 게임 오브젝트가 Player 태그를 가진 경우
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();//상대방 게임 오브젝트에서 PlayerController 컴포넌트 가져오기
+
+            if(playerController != null)// 상대방으로부터 playerController 컴포넌트를 가져오는데 성공했다면
+            {
+                playerController.Die(); //상대방 PlayerController 컴포넌트의 Die() 메서드 실행
+            }
+        }
     }
 }
