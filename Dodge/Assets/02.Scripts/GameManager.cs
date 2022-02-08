e40ec8535b93b573b05a2085df34eb7619a429e9
gwijong <drophail@daum.net>
2022-02-08 10:49:44 +0900

    20220208

---
+++ Dodge/Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameoverText;
+    public Text timeText;
+    public Text recordText;
+
+    private float surviveTime;
+    private bool isGameover;
+
+    void Start()
+    {
+        surviveTime = 0;
+        isGameover = false;
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
